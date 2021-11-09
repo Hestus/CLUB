2229ebff915a7c255e288d7982a81e91e8bd56c2
hestus <hestushestus@gmail.com>
2021-11-09 23:49:24 +0530

    added composition API to the homepage component

--- Pages/HomePage/HomePage.vue
+++ Pages/HomePage/HomePage.vue
@@ -53,80 +53,109 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import ContextMenu from "../../components/ContextMenu/ContextMenu.vue";
 import ContextMenuFolder from "../../components/ContextMenu/ContextMenuFolder.vue";
 import NewFolder from "../../components/NewFolder/NewFolder.vue";
 
 export default {
   components: { ContextMenu, NewFolder, ContextMenuFolder },
-  data() {
-    return {
-      // Opening Context Menu
-      context_menu: false,
-      context_menu_folder: false,
-      context_menu_taskbar: false,
-      // mouse click location
-      mouseX: 0,
-      mouseY: 0,
-      // Folders
-      folders: [
-        {
-          id: 1,
-          img: "https://i.ibb.co/g60bQdw/file-home-logo.png",
-          folder_name: "Desktop",
-        },
-        {
-          id: 2,
-          img: "https://i.ibb.co/g60bQdw/file-home-logo.png",
-          folder_name: "New Folder",
-        },
-      ],
-      // Create new folder
-      CreateNewFolder: false,
-    };
-  },
-  methods: {
+
+  setup() {
+    // Opening Context Menu
+    const context_menu = ref(false);
+    const context_menu_folder = ref(false);
+    const context_menu_taskbar = ref(false);
+    //     // mouse click location
+    const mouseX = ref("0");
+    const mouseY = ref("0");
+    // Folders
+    const folders = ref([
+      {
+        id: 1,
+        img: "https://i.ibb.co/g60bQdw/file-home-logo.png",
+        folder_name: "Desktop",
+      },
+      {
+        id: 2,
+        img: "https://i.ibb.co/g60bQdw/file-home-logo.png",
+        folder_name: "New Folder",
+      },
+    ]);
+
+    // Create new folder
+    const CreateNewFolder = ref(false);
+
     // Right Click on homepage Handler
-    handler(e) {
-      if (this.context_menu_folder === false) {
-        this.context_menu = true;
-        this.mouseX = e.clientX + "px";
-        this.mouseY = e.clientY - 60 + "px";
-        console.log(this.mouseX, this.mouseY);
+    const handler = (e) => {
+      if (context_menu_folder.value === false) {
+        context_menu.value = true;
+        mouseX.value = e.clientX + "px";
+        mouseY.value = e.clientY - 60 + "px";
+        console.log(mouseX.value, mouseY.value);
         e.preventDefault();
       }
-    },
+    };
+
     // Right Click over folder handler
-    handlerFolder(e) {
-      this.context_menu = false;
-      this.context_menu_folder = true;
-      this.mouseX = e.clientX + "px";
-      this.mouseY = e.clientY - 60 + "px";
-      console.log(this.mouseX, this.mouseY);
+    const handlerFolder = (e) => {
+      context_menu.value = false;
+      context_menu_folder.value = true;
+      mouseX.value = e.clientX + "px";
+      mouseY.value = e.clientY - 60 + "px";
+      console.log(mouseX.value, mouseY.value);
       e.preventDefault();
-    },
-    closeHandler() {
-      this.context_menu = false;
-      this.context_menu_folder = false;
-    },
-    CreatedNewFolder(foldercreateValue) {
-      this.CreateNewFolder = foldercreateValue;
+    };
+
+    // Closing Handler
+    const closeHandler = () => {
+      context_menu.value = false;
+      context_menu_folder.value = false;
+    };
+
+    // Create new folder
+    const CreatedNewFolder = (foldercreateValue) => {
+      CreateNewFolder.value = foldercreateValue;
       console.log(this.CreateNewFolder);
-    },
-    closedNewFolder(folderCloseValue) {
-      this.CreateNewFolder = folderCloseValue;
-      console.log(this.CreateNewFolder);
-    },
-    closedNewFolderEvent({ closingNewFolderBool, folder_new_name_now }) {
+    };
+
+    // closing new folder
+    const closedNewFolder = (folderCloseValue) => {
+      CreateNewFolder.value = folderCloseValue;
+      console.log(CreateNewFolder.value);
+    };
+
+    // Closing new folder event
+    const closedNewFolderEvent = ({
+      closingNewFolderBool,
+      folder_new_name_now,
+    }) => {
       console.log(closingNewFolderBool, folder_new_name_now);
-      this.CreateNewFolder = closingNewFolderBool;
-      this.folders.push({
+      CreateNewFolder.value = closingNewFolderBool;
+      folders.value.push({
         id: 3,
         img: "https://i.ibb.co/g60bQdw/file-home-logo.png",
         folder_name: folder_new_name_now,
       });
-    },
+    };
+
+    return {
+      context_menu,
+      context_menu_folder,
+      context_menu_taskbar,
+      mouseX,
+      mouseY,
+      folders,
+      CreateNewFolder,
+      handler,
+      handlerFolder,
+      closeHandler,
+      CreatedNewFolder,
+      closedNewFolder,
+      closedNewFolderEvent,
+    };
   },
+
 };
 </script>
 
---
+++ components/HomePageBackground/BackgroundChange.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="backgroundchange">
+    <div class="bgc_div">
+      <!-- Close new Folder Div -->
+      <!-- New Folder Title on top -->
+      <div class="bgc_top_title">
+        <span class="span-white ff_raleway">Folder Name</span>
+        <!-- Closing New Folder Menu  -->
+        <img
+          @click="closeImageUrlDiv()"
+          src="https://i.ibb.co/K2gtRpk/cross.png"
+          alt=""
+        />
+      </div>
+      <!-- input -->
+      <div class="bgc_input">
+        <input
+          type="url"
+          placeholder="Paste Image URL here"
+          v-model="bg_image_url"
+          @keyup.enter="handleSubmit()"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bg_image_url: "",
+    };
+  },
+  methods: {
+    closeImageUrlDiv() {
+      this.$emit("closeNewImageUrl", false);
+    },
+    handleSubmit() {
+      console.log(this.bg_image_url);
+      this.$emit("closeNewURLOnHandleSubmit", {
+        closingNewURLBool: false,
+        image_new_url_now: this.bg_image_url,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../CSS/backgroundchange.css";
+</style>
